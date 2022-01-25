220ca9e76cfbb7fbf71dcbde2a2b9d03ebdc6777
saurabh-gaonkar <saurabhg@axioned.com>
2022-01-25 14:49:51 +0530

    css updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
     $(".ham").click(function(){
         $(".menus").slideToggle(2000);
-        $(this).classList.toggle("cross");
+        $(this).removeClass("ham");
+        $(this).addClass("cross");
     });
+    $(".cross").click(function(){
+        $(this).addClass("ham");
+        $(this).removeClass("cross");
+       
+    })
 });
 
 
